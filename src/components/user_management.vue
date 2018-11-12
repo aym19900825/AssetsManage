096376854fb0d47f5ea8962bb4f34b5a96bd548a
heliping1989 <heliping_1989@126.com>
2018-11-12 18:01:42 +0800

    修改了table表格

--- src/components/user_management.vue
+++ src/components/user_management.vue
@@ -45,7 +45,7 @@
 							<v-table-controle :tableHeader="tableHeader" :checkedName="checkedName"  @tableControle="tableControle" ref="tableControle"></v-table-controle>
 						</div>
 					</div>
-					<!-- 高级查询划出 -->
+					<!-- 高级查询划出 Begin-->
 					<div v-show="search">
 						<el-form status-icon :model="searchList" label-width="70px">
 							<el-row :gutter="10">
@@ -70,7 +70,7 @@
 							</el-row>
 						</el-form>
 					</div>
-					<!-- 高级查询划出 -->
+					<!-- 高级查询划出 End-->
 					<div class="row">
 						<div class="col-sm-3">
 							<v-assetsTree  :listData="treeData" v-on:getTreeId="getTreeId"></v-assetsTree>
@@ -505,121 +505,5 @@
 </script>
 
 <style scoped>
-	.headerbg {
-		width: 100%;
-		height: 100px;
-		position: fixed;
-		z-index: 999;
-	}
-	
-	.contentbg {
-		padding-top: 100px;
-		height: 100%;
-	}
-	
-	.wrapper {
-		width: 100%;
-		height: 100%;
-		padding-left: 220px;
-		position: relative;
-	}
-	
-	.ibox-content {
-		background-color: #F5F8FB;
-		color: inherit;
-		padding: 0px 20px 20px 10px;
-		border-color: #e7eaec;
-		-webkit-border-image: none;
-		-o-border-image: none;
-		border-image: none;
-		border-style: solid solid none;
-		border-width: 1px 0px;
-	}
-	
-	[class*=" btn-"],
-	[class^="btn-"] {
-		color: #FFFFFF;
-		font-size: 14px;
-	}
-	
-	[class*=" btn-"] i,
-	[class^="btn-"] i {
-		margin-right: 5px;
-	}
-	
-	.btn-green {
-		background-color: #25b99e;
-		border-color: #25b99e;
-	}
-	
-	.btn-bule {
-		background-color: #2fa5e5;
-		border-color: #2fa5e5;
-	}
-	
-	.btn-red {
-		background-color: #e3517c;
-		border-color: #e3517c;
-	}
-	
-	.btn-primarys {
-		background-color: #5d7fde;
-		border-color: #5d7fde;
-	}
-	
-	.btn-default {
-		background-color: #ffffff;
-		color: #5B6371;
-		border: 1px solid #dfe5ea;
-		margin-right: -1px;
-	}
-	
-	.btn-default:hover {
-		color: #576FAE;
-	}
-	
-	.fixed-table-toolbar {
-		position: relative;
-		padding-top: 10px;
-		padding-bottom: 12px;
-		height: 55px;
-		line-height: 30px;
-	}
-	
-	.fixed-table-toolbar .columns {
-		position: relative;
-		height: 30px;
-	}
-	
-	.btn-group>.btn:first-child:not(:last-child):not(.dropdown-toggle) {
-		border-top-right-radius: 0;
-		border-bottom-right-radius: 0;
-	}
-	
-	.fixed-table-toolbar .btn-group>.btn-group {
-		display: inline-block;
-		margin-left: -1px!important;
-	}
-	
-	.btn-group>.btn-group,
-	.btn-group>.btn {
-		float: left;
-	}
-	
-	.btn-group,
-	.btn-group-vertical {
-		position: relative;
-		display: inline-block;
-		vertical-align: middle;
-	}
-	
-	.btn-group>.btn-group:last-child:not(:first-child)>.btn:first-child {
-		border-top-left-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-	
-	.fixed-table-toolbar .btn-group>.btn-group:last-child>.btn {
-		border-top-right-radius: 4px;
-		border-bottom-right-radius: 4px;
-	}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -46,3 +46,15 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+window.Vue.directive('loadmore', {
+  	bind(el, binding) {
+    	const selectWrap = el.querySelector('.el-table__body-wrapper')
+    	selectWrap.addEventListener('scroll', function() {
+     	    let sign = 100;
+     	    const scrollDistance = this.scrollHeight - this.scrollTop - this.clientHeight
+     	    if (scrollDistance <= sign) {
+     	      	binding.value();
+     	    }
+    	})
+  	}
+})

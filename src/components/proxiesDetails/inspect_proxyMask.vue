825c723e897617d0841085fbc190061780c333b8
kaixin1989 <976370787@qq.com>
2018-12-20 18:06:45 +0800

    添加检验委托书

--- src/components/proxiesDetails/inspect_proxyMask.vue
+++ src/components/proxiesDetails/inspect_proxyMask.vue
@@ -62,7 +62,7 @@
 											</el-form-item>
 										</el-col>
 									</el-row>
-									<el-row >
+									<el-row :gutter="5">
 										<el-col :span="8">
 											<el-form-item label="姓名" prop="V_PERSON" label-width="110px">
 												<el-input v-model="dataInfo.V_PERSON" :disabled="true">
@@ -405,8 +405,8 @@
 							
 											<el-table-column prop="CHECKCOST" label="检验检测费用" sortable width="120px">
 												<template slot-scope="scope">
-													<el-form-item :prop="'CHECK_PROXY_CONTRACTList.'+scope.$index + '.Q_TYPE'" :rules="[{required: true, message: '请输入', trigger: 'blur'}]" >
-													<el-input v-if="scope.row.isEditing" size="small" v-model="scope.row.CHECKCOST" placeholder="请输入内容"></el-input>
+													<el-form-item :prop="'CHECK_PROXY_CONTRACTList.'+scope.$index + '.CHECKCOST'" :rules="[{required: true, message: '请输入', trigger: 'blur'}]" >
+													<el-input id='checkcost' v-if="scope.row.isEditing" size="small" v-model="scope.row.CHECKCOST" placeholder="请输入内容" @change='handleChange(scope.row)'></el-input>
 													<span v-else="v-else">{{scope.row.CHECKCOST}}</span>
 													</el-form-item>
 												</template>
@@ -527,7 +527,7 @@
 										</el-col>
 									    <el-col :span="16">
 										<el-form-item label="生产单位名称" prop="P_NAME" label-width="110px">
-											<el-input v-model="dataInfo.P_NAME" :disabled="true" >
+											<el-input v-model="dataInfo.P_NAME" :disabled="false" >
 												<el-button slot="append" icon="el-icon-search" @click="getCustomer(2)"></el-button>
 											</el-input>
 										</el-form-item>
@@ -748,6 +748,15 @@
 				// this.dataInfo.CHECTCOST="￥" + num.join(".");
 				this.dataInfo.ACTUALCOST = num.join(".");
 			},
+			handleChange(item){
+				console.log(item)
+				var money = document.getElementById("checkcost").value;
+				this.initcost = money;
+				var num = parseFloat(this.toNum(money)).toFixed(2).toString().split(".");
+				num[0] = num[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)','ig'),"$1,");
+				// this.dataInfo.CHECTCOST="￥" + num.join(".");
+				item.CHECKCOST = num.join(".");
+			},
 			reset() {					
 				this.dataInfo={
 					VERSION: '1',
@@ -914,13 +923,6 @@
 					this.dataInfo.CONTRACTCOST = this.initsta;
 					this.dataInfo.ACTUALCOST = this.initactual;
 			        if (valid) {
-							if(this.dataInfo.INSPECT_PROXY_BASISList.length<=0&&this.dataInfo.INSPECT_PROXY_PROJECList.length<=0&&this.dataInfo.CHECK_PROXY_CONTRACTList.length<=0){
-			        		this.$message({
-							message: '检验依据和检验项目与要求和分包要求是必填项，请填写！',
-							type: 'warning'
-						});
-						return false;
-			        	}else{
 			        this.dataInfo.ITEM_STATUS=this.dataInfo.ITEM_STATUS==1;
 			        this.dataInfo.MESSSTATUS= this.dataInfo.MESSSTATUS==1;//信息状态
 			        console.log(this.dataInfo);
@@ -942,11 +944,11 @@
 							message: '网络错误，请重试',
 							type: 'error'
 						});
-					});}
+					});
 			          } else {
 			           this.$message({
 							message: '未填写完整，请填写',
-							type: 'warning'
+							type: 'error'
 						});
 			          }
 				});
@@ -996,7 +998,6 @@
 				this.dataInfo.V_PHONE = this.selval[0].PHONE;
 				} else {
 					this.dataInfo.PRODUCT_UNIT= this.selval[0].CODE;
-					console.log(this.dataInfo.PRODUCT_UNIT);
 					this.dataInfo.P_NAME = this.selval[0].NAME;
 				}
 			},

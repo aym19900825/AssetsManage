44d8462f5333195ac0fc8528d5932fab96343f73
825185228@qq.com <825185228@qq.com>
2018-12-25 17:26:46 +0800

    文件的上传

--- src/components/common/doc.vue
+++ src/components/common/doc.vue
@@ -1,14 +1,15 @@
 <template>
 <div>
     <div class="table-func">
-        <el-button type="warn" size="mini" round @click="upload">
+        <!-- <el-button type="warn" size="mini" round @click="upload">
             <i class="el-icon-upload"></i>
             <font>上传</font>
-        </el-button>
+        </el-button> -->
         <!-- <el-button type="success" size="mini" round @click="addLine">
             <i class="icon-add"></i>
             <font>新建行</font>
         </el-button> -->
+        
         <el-button type="error" size="mini" round>
             <i class="el-icon-delete"></i>
             <font>删除行</font>
@@ -26,6 +27,14 @@
         <el-table-column prop="filesize" label="文档大小" sortable >
         </el-table-column>
     </el-table>
+    <form method="post" id="file" action="" enctype="multipart/form-data">
+        <div class="a-upload">
+            文件上传
+            <input id="excelFile" type="file" name="uploadFile"/>
+            <br/><br/>
+        </div>
+        <input type="button" value="上传" @click="upload"/>
+    </form>
 </div>
 </template>
 
@@ -36,14 +45,28 @@ export default {
       data(){
         return {
             basic_url: Config.dev_url,
-            doc: []
+            doc: [],
+            fileList: []
         }
     },
     methods: {
-        addLine(){
-            this.doc.push({
-
-            });
+        upload(e){
+            var formData = new FormData();
+            formData.append('files', document.getElementById('excelFile').files[0]);
+            var config = {
+                //添加请求头
+                headers: { "Content-Type": "multipart/form-data" },
+                //添加上传进度监听事件
+                onUploadProgress: e => {
+                    var completeProgress = ((e.loaded / e.total * 100) | 0) + "%";
+                    this.progress = completeProgress;
+                }
+            };
+            var url = 'http://192.168.1.140:8060/file/uploadfile?userid=66&username=user&deptid=101&deptfullname=workorder&recordid=1&appname=workorder&appid=99';
+            this.$axios.post(url, formData, config
+            ).then((res)=>{
+                console.log(res)
+            })
         },
         getData(){
             var url = 'http://192.168.1.140:8060/file/fileList?page=0&size=10';
@@ -67,5 +90,33 @@ export default {
 </script>
 
 <style scoped>  
-
+.a-upload{
+    padding: 4px 10px;
+    width: 85px;
+    height: 34px;
+    line-height: 28px;
+    position: relative;
+    cursor: pointer;
+    color: #fff;
+    background-color: #286090;
+    border-color: #204d74;
+    border-radius: 4px;
+    overflow: hidden;
+    display: block;
+}
+.a-upload input{
+    position: absolute;
+    font-size: 100px;
+    right: 0;
+    top: 0;
+    opacity: 0;
+    filter: alpha(opacity=0);
+    cursor: pointer
+}
+.a-upload:hover{
+    color: #FFFFFF;
+    background: #337ab7;
+    border-color: #204d74;
+    text-decoration: none;
+}
 </style>

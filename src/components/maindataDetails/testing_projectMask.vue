edd1b9a38698f4210c8becc9014b03ca2e9d7d70
pangsiqi <746609995@qq.com>
2019-03-01 16:12:37 +0800

    检验/检测项目选择作业指导书

--- src/components/maindataDetails/testing_projectMask.vue
+++ src/components/maindataDetails/testing_projectMask.vue
@@ -859,17 +859,17 @@
 			},
 			addwork() { //小弹出框确认按钮事件
 				console.log(this.selval.length);
-				// if(this.selval.length == 0){
-				// 	this.$message({
-				// 		message: '请选择数据',
-				// 		type: 'warning'
-				// 	});
-				// }else if(this.selval.length>1){
-				// 	this.$message({
-				// 		message: '不可以同时选择多条数据',
-				// 		type: 'warning'
-				// 	});
-				// }else{
+				if(this.selval.length == 0){
+					this.$message({
+						message: '请选择数据',
+						type: 'warning'
+					});
+				}else if(this.selval.length>1){
+					this.$message({
+						message: '不可以同时选择多条数据',
+						type: 'warning'
+					});
+				}else{
 					var changeUser = this.selval;
 					var list = [];
 					//basisnum为依据编号的数组
@@ -879,15 +879,14 @@
 					}
 					//basisnums为basisnum数组用逗号拼接的字符串el-tag__close el-icon-close
 					this.testing_projectForm.DOCLINKS_NUM = basisdesc.toString(',');
-					console.log(this.testing_projectForm.DOCLINKS_NUM)
 					// list.push(basisnums);
 					// for(var i = 0;i<this.selUser.length;i++){
 					// 	this.selUser[i].ID = '';
 					// 	list.push(this.selUser[i]);
 					// }
 
-					// this.dialogVisible2 = false;
-				// }
+					this.dialogVisible2 = false;
+				}
 			},
 			handleClose(done) {
 				this.$confirm('确认关闭？')

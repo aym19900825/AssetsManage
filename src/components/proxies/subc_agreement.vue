296c2d268db16e9a559ef1b9e1c836a5447e5606
kaixin1989 <976370787@qq.com>
2019-03-03 14:50:16 +0800

    委托书的按钮

--- src/components/proxies/subc_agreement.vue
+++ src/components/proxies/subc_agreement.vue
@@ -15,7 +15,10 @@
 				<div class="fixed-table-toolbar clearfix">
 					<div class="bs-bars pull-left">
 						<div class="hidden-xs" id="roleTableToolbar" role="group">
-							<button type="button" class="btn btn-blue button-margin" @click="modify">
+							<button v-for="item in buttons" class="btn mr5" :class="item.style" @click="getbtn(item)">
+									<i :class="item.icon"></i>{{item.name}}
+							</button>
+							<!-- <button type="button" class="btn btn-blue button-margin" @click="modify">
 							    <i class="icon-edit"></i>修改
 							</button>
 							<button type="button" class="btn btn-primarys button-margin" @click="exportData">
@@ -31,7 +34,7 @@
 					    		<i class="icon-search"></i>高级查询
 					    		<i class="icon-arrow1-down" v-show="down"></i>
 					    		<i class="icon-arrow1-up" v-show="up"></i>
-							</button>
+							</button> -->
 						</div>
 					</div>
 					<div class="columns columns-right btn-group pull-right">
@@ -299,8 +302,6 @@
 				companyId: '',
 				deptId: '',
 				selUser: [],
-				// '启用': true,
-				// '冻结': false,
 				subagree: [],
 				search: false,
 				show: false,
@@ -331,20 +332,10 @@
 					pageSize: 10,
 					totalCount: 0
 				},
-				aaaData:[],
+				buttons:[],
 			}
 		},
-
-		mounted(){
-			// 获取浏览器可视区域高度
-			window.onresize = () => {//获取浏览器可视区域高度
-			 	return (() => {
-			 		this.fullHeight.height = document.documentElement.clientHeight - 100+'px';
-			 	})()
-		 	};	
-		},
 		methods: {
-			
 			//表头居中
 			rowClass({ row, rowIndex}) {
 			    return 'text-align:center'
@@ -380,8 +371,20 @@
 				this.down = !this.down,
 				this.up = !this.up
 			},
-			openAddMgr(){
-
+			getbtn(item){
+				if(item.name=="修改"){
+					this.modify();
+				}else if(item.name=="高级查询"){
+					this.modestsearch();
+				}else if(item.name=="导入"){
+				this.download();
+				}else if(item.name=="导出"){
+					this.download();
+				}else if(item.name=="报表"){
+					this.reportdata();
+				}else if(item.name=="打印"){
+					this.Printing();
+				}
 			},
 			modify() {
 				if(this.selUser.length == 0) {
@@ -404,53 +407,6 @@
 			 view(id) {
 				this.$refs.child.view(id);
 			},
-			// 删除
-			deluserinfo() {
-				var selData = this.selUser;
-				if(selData.length == 0) {
-					this.$message({
-						message: '请您选择要删除的用户',
-						type: 'warning'
-					});
-					return;
-				} else {
-					var url = this.basic_url + '/api-apps/app/checkProCont2/deletes';
-					//changeUser为勾选的数据
-					var changeUser = selData;
-					//deleteid为id的数组
-					var deleteid = [];
-					var ids;
-					for (var i = 0; i < changeUser.length; i++) {
-						deleteid.push(changeUser[i].ID);
-					}
-					//ids为deleteid数组用逗号拼接的字符串
-					ids = deleteid.toString(',');
-                    var data = {
-						ids: ids,
-					}
-					this.$confirm('确定删除该条信息吗？', '提示', {
-                        confirmButtonText: '确定',
-                        cancelButtonText: '取消',
-                    }).then(({ value }) => {
-                        this.$axios.delete(url, {params: data}).then((res) => {
-							if(res.data.resp_code == 0) {
-								this.$message({
-									message: '删除成功',
-									type: 'success'
-								});
-								this.requestData();
-							}
-						}).catch((err) => {
-							this.$message({
-								message: '网络错误，请重试',
-								type: 'error'
-							});
-						});
-                    }).catch(() => {
-
-                	});
-				}
-			},
 			// 导入
 			importData() {
 				
@@ -459,7 +415,7 @@
 			exportData() {
 				
 			},
-						//报表
+			//报表
 			reportdata(){
 				this.reportData.app=this.productType;
 				this.$refs.reportChild.visible();
@@ -468,14 +424,6 @@
 			Printing() {
 				
 			},
-			//下发协议
-			distribute(){
-
-			},
-			// judge(data) {
-			// 	//taxStatus 布尔值
-			// 	return data.enabled ? '启用' : '冻结'
-			// },
 			//时间格式化  
 			dateFormat(row, column) {
 				var date = row[column.property];
@@ -595,8 +543,29 @@
 			},
 			childByValue:function(childValue) {
         		// childValue就是子组件传过来的值
-        		this.$refs.navsheader.showClick(childValue);
-      		},
+				this.$refs.navsheader.showClick(childValue);
+				this.getbutton(childValue);
+			},
+			  //请求页面的button接口
+		    getbutton(childByValue){
+		    	console.log(childByValue);
+		    	var data = {
+					menuId: childByValue.id,
+					roleId: this.$store.state.roleid,
+				};
+				var url = this.basic_url + '/api-user/permissions/getPermissionByRoleIdAndSecondMenu';
+				this.$axios.get(url, {params: data}).then((res) => {
+					console.log(res);
+					this.buttons = res.data;
+					
+				}).catch((wrong) => {
+					this.$message({
+								message: '网络错误，请重试',
+								type: 'error'
+							});
+				})
+
+		    },
 		},
 		mounted(){
              // 注册scroll事件并监听  

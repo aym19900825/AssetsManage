0cac1d69645f9f54c63c1e1617d3254121e61a6e
825185228@qq.com <825185228@qq.com>
2019-03-19 20:29:21 +0800

    业务基础数据模块的table的抽取

--- src/components/maindata/work_instruction.vue
+++ src/components/maindata/work_instruction.vue
@@ -61,21 +61,7 @@
 					<el-row :gutter="0">
 						<el-col :span="24">
 							<!-- 表格 Begin-->
-							<el-table ref="table" :header-cell-style="rowClass" 
-								  :data="WORK_INSTRUCTIONList" 
-								  border 
-								  stripe 
-								  :height="fullHeight" 
-								  style="width: 100%;" 
-								  :default-sort="{prop:'WORK_INSTRUCTIONList', order: 'descending'}"
-								   @selection-change="SelChange" 
-								   v-loadmore="loadMore"
-								   v-loading="loading"  
-								   element-loading-text="加载中…"
-								   element-loading-spinner="el-icon-loading"
-								   element-loading-background="rgba(255, 255, 255, 0.9)">
-								<el-table-column type="selection" fixed width="55" v-if="this.checkedName.length>0" align="center">
-								</el-table-column>
+							<v-table ref="table" :appName="appName" :searchList="searchList" @getSelData="setSelData">
 								<el-table-column label="分发号" width="155" sortable prop="NUM" v-if="this.checkedName.indexOf('分发号')!=-1">
 									<template slot-scope="scope">
 										<p class="blue" title="点击查看详情" @click=view(scope.row)>{{scope.row.NUM}}
@@ -93,9 +79,7 @@
 								</el-table-column>
 								<el-table-column label="机构" width="120" prop="DEPTIDDesc" sortable v-if="this.checkedName.indexOf('机构')!=-1">
 								</el-table-column>
-							</el-table>
-							<el-pagination background class="text-right pt10" v-if="this.checkedName.length>0" @size-change="sizeChange" @current-change="currentChange" :current-page="page.currentPage" :page-sizes="[10, 20, 30, 40,100]" :page-size="page.pageSize" layout="total, sizes, prev, pager, next" :total="page.totalCount">
-							</el-pagination>
+							</v-table>
 							<!-- 表格 End-->
 						</el-col>
 					</el-row>
@@ -116,18 +100,21 @@
 	import instructionmask from '../maindataDetails/work_instructionMask.vue'
 	import tableControle from '../plugin/table-controle/controle.vue'
 	import reportmask from'../reportDetails/reportMask.vue'
+	import vTable from '../plugin/table/table.vue'
 	export default {
 		name: 'customer_management',
 		components: {
-			vheader,
-			navs_left,
-			navs_tabs,
-			instructionmask,
-			tableControle,
-			reportmask
+			'vheader': vheader,
+			'navs_left': navs_left,
+			'navs_tabs': navs_tabs,
+			'instructionmask': instructionmask,
+			'tableControle': tableControle,
+			'reportmask': reportmask,
+			'v-table': vTable
 		},
 		data() {
 			return {
+				appName: 'workIns',
 				btn:'',
 				reportData:{},//报表的数据
 				basic_url: Config.dev_url,
@@ -202,9 +189,8 @@
 			}
 		},
 		methods: {
-			//表头居中
-			rowClass({ row, rowIndex}) {
-			    return 'text-align:center'
+			setSelData(val){
+				this.selUser = val;
 			},
 			//机构值
 			getCompany() {
@@ -218,60 +204,8 @@
 					this.selectData = res.data;
 				});
 			},
-			//表格滚动加载
-			loadMore() {
-				let up2down = sessionStorage.getItem('up2down');
-				if(this.loadSign) {					
-					if(up2down=='down'){
-						this.page.currentPage++;
-						if(this.page.currentPage > Math.ceil(this.page.totalCount / this.page.pageSize)) {
-							this.page.currentPage = Math.ceil(this.page.totalCount / this.page.pageSize)
-							return false;
-						}
-						let append_height = window.innerHeight - this.$refs.table.$el.offsetTop - 50;
-						if(this.page.currentPage == Math.ceil(this.page.totalCount / this.page.pageSize)){
-							$('.el-table__body-wrapper table').append('<div class="filing" style="height: '+append_height+'px;width: 100%;"></div>');
-							sessionStorage.setItem('toBtm','true');
-						}
-					}else{
-						sessionStorage.setItem('toBtm','false');
-						this.page.currentPage--;
-						if(this.page.currentPage < 1) {
-							this.page.currentPage=1;
-							return false;
-						}
-					}
-					this.loadSign = false;
-					setTimeout(() => {
-						this.loadSign = true;
-					}, 1000)
-					this.requestData();
-				}
-			},
 			tableControle(data) {
 				this.checkedName = data;
-			},
-			//改变页数
-			sizeChange(val) {
-				this.page.pageSize = val;
-				if(this.page.currentPage == Math.ceil(this.page.totalCount / this.page.pageSize)){
-					$('.el-table__body-wrapper table').append('<div class="filing" style="height: 800px;width: 100%;"></div>');
-					sessionStorage.setItem('toBtm','true');
-				}else{
-					sessionStorage.setItem('toBtm','false');
-				}
-				this.requestData();
-			},
-			//当前页数
-			currentChange(val) {
-				this.page.currentPage = val;
-				if(this.page.currentPage == Math.ceil(this.page.totalCount / this.page.pageSize)){
-					$('.el-table__body-wrapper table').append('<div class="filing" style="height: 800px;width: 100%;"></div>');
-					sessionStorage.setItem('toBtm','true');
-				}else{
-					sessionStorage.setItem('toBtm','false');
-				}
-				this.requestData();
 			},
 			resetbtn(){
 				this.searchList =  { //点击高级搜索后显示的内容
@@ -279,12 +213,10 @@
 					VERSION:'',
 					DEPTID:''
 				};
-				this.requestData();
+				this.requestData('init');
 			},
-			searchinfo(index) {
-				this.page.currentPage = 1;
-				this.page.pageSize = 20;
-				this.requestData();
+			searchinfo() {
+				this.requestData('init');
 			},
 			//清空
 			reset() {
@@ -523,9 +455,6 @@
 				this.$refs.reportChild.visible();
 			},
 
-			judge(data) {
-				data.STATUS = data.STATUS == "1" ? '活动' : '不活动'
-			},
 			//时间格式化  
 			dateFormat(row, column) {
 				var date = row[column.property];
@@ -534,42 +463,9 @@
 				}
 				return this.$moment(date).format("YYYY-MM-DD");
 			},
-			SelChange(val) {
-				this.selUser = val;
-			},
 			//Table默认加载数据
-			requestData() {
-				this.loading = true;//加载动画打开
-				var data = {
-					page: this.page.currentPage,
-					limit: this.page.pageSize,
-					DESCRIPTION: this.searchList.DESCRIPTION,
-					VERSION:this.searchList.VERSION,
-					DEPTID: this.searchList.DEPTID,
-				}
-				var url = this.basic_url + '/api-apps/app/workIns';
-				this.$axios.get(url, {
-					params: data
-				}).then((res) => {
-					this.page.totalCount = res.data.count;
-					//总的页数
-					let totalPage = Math.ceil(this.page.totalCount / this.page.pageSize)
-					if(this.page.currentPage >= totalPage) {
-						this.loadSign = false
-					} else {
-						this.loadSign = true
-					}
-					this.WORK_INSTRUCTIONList = res.data.data;
-					this.loading = false;//加载动画关闭
-					if($('.el-table__body-wrapper table').find('.filing').length>0 && this.page.currentPage < totalPage){
-						$('.el-table__body-wrapper table').find('.filing').remove();
-					}//滚动加载数据判断filing
-				}).catch((wrong) => {
-				})
-			},
-			
-			formatter(row, column) {
-				return row.enabled;
+			requestData(opt) {
+				this.$refs.table.requestData(opt);
 			},
 			childByValue:function(childValue) {
         		// childValue就是子组件传过来的值
@@ -601,7 +497,6 @@
 			}
 		},
 		mounted() {
-			this.requestData();
 			this.getCompany();
 			this.getdept();
 		},

fd90c4e7caee1ab0bac23b344844aad351c86b38
kaixin1989 <976370787@qq.com>
2019-03-21 15:17:15 +0800

    所有有所属机构的小按钮点击事件删除

--- src/components/annual_plan.vue
+++ src/components/annual_plan.vue
@@ -137,9 +137,9 @@
 							<div class="lefttreebg">
 								<div class="left_tree_title clearfix" @click="min3max()">
 									<div class="pull-left pr20" v-if="ismin">类型</div>
-									<span class="pull-right navbar-minimalize minimalize-styl-2">
+									<!-- <span class="pull-right navbar-minimalize minimalize-styl-2">
 										<i class="icon-doubleok icon-double-angle-left blue"></i>
-									</span>
+									</span> -->
 								</div>
 								<div class="left_treebg" :style="{height: fullHeight}">
 									<div class="p15" v-if="ismin">

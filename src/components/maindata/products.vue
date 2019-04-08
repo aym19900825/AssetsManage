fbfcce2f6c71cc7daea28b91c84d9d2fe5980f70
kaixin1989 <976370787@qq.com>
2019-04-08 16:56:47 +0800

    业务基础数据的删除高级查询的冗余代码

--- src/components/maindata/products.vue
+++ src/components/maindata/products.vue
@@ -198,8 +198,6 @@
 				productList: [],
 				search: false,
 				show: false,
-				down: true,
-				up: false,
 				isShow: false,
 				ismin: true,
 				fullHeight: document.documentElement.clientHeight - 210 + 'px', //获取浏览器高度
@@ -372,8 +370,6 @@
 			//高级查询
 			modestsearch() {
 				this.search = !this.search;
-				this.down = !this.down,
-				this.up = !this.up
 			},
 			// 删除
 			deluserinfo() {
--- src/components/maindata/testing_projects.vue
+++ src/components/maindata/testing_projects.vue
@@ -249,8 +249,6 @@
 				projectList: [],
 				search: false,
 				show: false,
-				down: true,
-				up: false,
 				isShow: false,
 				ismin:true,
 				fullHeight: document.documentElement.clientHeight - 210+'px',//获取浏览器高度
@@ -421,8 +419,6 @@
 			//高级查询
 			modestsearch() {
 				this.search = !this.search;
-				this.down = !this.down,
-				this.up = !this.up
 			},
 			// 删除
 			deluserinfo() {
